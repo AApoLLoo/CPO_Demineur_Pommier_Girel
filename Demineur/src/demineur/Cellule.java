cef27eb7a8e2a1cffbc390420299ff398d467cb8
danie <danie@10.3.61.142>
2024-11-20 10:59:41 +0100

    CPO_miniprojet_démineur : Classe Cellule définition Methode Constructeur

---
+++ Demineur/src/demineur/Cellule.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package demineur;
+
+/**
+ *Cellule Demineur
+ * @author danie
+ */
+public class Cellule {
+    boolean Bombe ;
+    boolean reveal;
+    int voisin;
+//Constructeur
+public Cellule(boolean Bombe){
+this.Bombe=Bombe;
+this.reveal=false;
+this.voisin=0;
+}
+
+//Methode
+public String isreveal (){
+    this.reveal=true;
+    if (Bombe=true){
+        return "il y a une bombe";
+    }else{
+        return String.valueOf(voisin);
+            
+        }
+    }    
+public void ajouterbombe (){
+    this.voisin++;
+}
+//Accesseurs 
+public boolean  Bombe(){
+    return Bombe ;   
+}       
+        
+public boolean reveal(){
+    return reveal;
+}
+
+public int voisin(){  
+    return voisin ;
+}
+
+
+}
+
+    
