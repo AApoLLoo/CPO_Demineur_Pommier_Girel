d8669cbcd1c76cb2f4ee8dff38d480df12a4b975
Antoine Pommier <antoine.pommier@epfedu.fr>
2024-11-27 11:09:29 +0100

    Démineur terminé avec choix possible au démarrage du programme

--- Demineur/src/demineur/Partie.java
+++ Demineur/src/demineur/Partie.java
@@ -8,11 +8,11 @@ import java.util.Scanner;
 /**
  * PARTIE
  *
- * @author danie
+ * @author Antoine Girel/Pommier
  */
 
     public class Partie {
-    private GrilleDeJeu grille;
+    private final GrilleDeJeu grille;
     private boolean enCours;
 
     public Partie(int lignes, int colonnes, int nombreDeBombes) {
@@ -24,23 +24,43 @@ import java.util.Scanner;
         Scanner scanner = new Scanner(System.in);
 
         while (enCours) {
-            grille.afficherGrille();
-            System.out.print("Entrez la ligne et la colonne à révéler (ex: 0 1): ");
-            int ligne = scanner.nextInt();
-            int colonne = scanner.nextInt();
-
-            if (grille.estBombe(ligne, colonne)) {
-                System.out.println("BOUM! Vous avez cliqué sur une bombe. Vous avez perdu.");
-                enCours = false;
-            } else {
-                grille.revelerCellule(ligne, colonne);
-                if (grille.aGagne()) {
-                    System.out.println("Félicitations! Vous avez révélé toutes les cellules sans bombe. Vous avez gagné!");
+            System.out.println("Options :");
+            System.out.println("1. Révéler une cellule");
+            System.out.println("2. Afficher le nombre de vies restantes");
+            System.out.println("3. Afficher la grille");
+            System.out.println("4. Quitter la partie");
+            System.out.print("Entrez votre choix : ");
+            int choice = scanner.nextInt();
+            switch(choice) {
+                case 1:
+                    System.out.print("Entrez la ligne et la colonne à révéler (ex: 0 1): ");
+                    int ligne = scanner.nextInt();
+                    int colonne = scanner.nextInt();
+                    if (grille.estBombe(ligne, colonne)) {
+                        System.out.println("BOUM! Vous avez cliqué sur une bombe. Vous avez perdu.");
+                        enCours = false;
+                    } else {
+                        grille.revelerCellule(ligne, colonne);
+                        if (grille.aGagne()) {
+                            System.out.println("Félicitations! Vous avez révélé toutes les cellules sans bombe. Vous avez gagné!");
+                            enCours = false;
+                        }
+                    }
+                    grille.afficherGrille();
+                    break;
+                case 2:
+                    System.out.println("Il vous reste "); //+ grille.getVies() + " vies.");
+                    break;
+                case 3:
+                    grille.afficherGrille();
+                    break;
+                case 4:
                     enCours = false;
-                }
+                    break;
+                default:
+                    System.out.println("Choix invalide.");
             }
-        }
-
+            }
         grille.afficherGrille();
         scanner.close();
     }
